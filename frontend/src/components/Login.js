2054e4f08f9d0183e568ae0d6514f282268ea648
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-03-29 18:58:26 +0530

    "npm start" error fix + some warnings removed

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,37 +1,42 @@
-import React from 'react';
-import './App.css';
-import img1 from './img/logo.jpeg';
+import "./style/App.css";
+import img1 from "./media/logo.jpeg";
 function Login() {
   return (
     <div className="App" id="page">
-    <div id="firstdiv">
-       <section id="head">
-       <h1><b>KARO STARTUP </b></h1>
-       <p>Get internship in less than 10 minutes!</p>
-       </section>
-       <section id="image">
-         <img src={img1}/>
-       </section>
-    </div>
-    <div class="box">
-      <form  class="form" action="">
-         <div id="boxess">
-           <input type="text"  placeholder="Enter your name" id="name"/>
-         </div>
-         <div id="boxess">
-           <input type="email"  id="name" placeholder="Enter your email"/>
-         </div>
-         <div id="boxess">
-           <input type="password"  id="name" placeholder="Enter your password"/>
-         </div>
-         <div id="boxess">
-           <input type="password"  id="name" placeholder="confirm  password"/>
-         </div>
-         <div>
-           <input type="submit" value="Login" id="submit"/>
-         </div>
-       </form>
-    </div>
+      <div id="firstdiv">
+        <section id="head">
+          <h1>
+            <b>KARO STARTUP </b>
+          </h1>
+          <p>Get internship in less than 10 minutes!</p>
+        </section>
+        <section id="image">
+          <img src={img1} alt="logo" />
+        </section>
+      </div>
+      <div class="box">
+        <form class="form" action="">
+          <div id="boxess">
+            <input type="text" placeholder="Enter your name" id="name" />
+          </div>
+          <div id="boxess">
+            <input type="email" id="name" placeholder="Enter your email" />
+          </div>
+          <div id="boxess">
+            <input
+              type="password"
+              id="name"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div id="boxess">
+            <input type="password" id="name" placeholder="confirm  password" />
+          </div>
+          <div>
+            <input type="submit" value="Login" id="submit" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,43 +1,56 @@
-import React from 'react';
-import './App.css';
-import img1 from './img/logo.jpeg';
+import "./style/App.css";
+import img1 from "./media/logo.jpeg";
 function Signup() {
   return (
     <div className="App" id="page">
-    <div id="firstdiv">
-       <section id="head">
-       <h1><b>KARO STARTUP </b></h1>
-       <p>Get internship in less than 10 minutes!</p>
-       </section>
-       <section id="image">
-         <img src={img1}/>
-       </section>
-    </div>
-    <div class="box">
-      <form  class="form" action="">
-         <div id="boxes">
-           <input type="text"  placeholder="Enter your name" id="name"/>
-         </div>
-         <div id="boxes">
-           <input type="text"  id="name" placeholder="Enter your username "/>
-         </div>
-         <div id="boxes">
-           <input type="number"  id="name" placeholder="Enter your contact number"/>
-         </div>
-         <div id="boxes">
-           <input type="email"  id="name" placeholder="Enter your email"/>
-         </div>
-         <div id="boxes">
-           <input type="password"  id="name" placeholder="Enter your password"/>
-         </div>
-         <div id="boxes">
-           <input type="password"  id="name" placeholder="please re-enter the password"/>
-         </div>
-         <div>
-           <input type="submit" value="SIGN UP" id="submit"/>
-         </div>
-       </form>
-    </div>
+      <div id="firstdiv">
+        <section id="head">
+          <h1>
+            <b>KARO STARTUP </b>
+          </h1>
+          <p>Get internship in less than 10 minutes!</p>
+        </section>
+        <section id="image">
+          <img src={img1} alt="logo" />
+        </section>
+      </div>
+      <div class="box">
+        <form class="form" action="">
+          <div id="boxes">
+            <input type="text" placeholder="Enter your name" id="name" />
+          </div>
+          <div id="boxes">
+            <input type="text" id="name" placeholder="Enter your username " />
+          </div>
+          <div id="boxes">
+            <input
+              type="number"
+              id="name"
+              placeholder="Enter your contact number"
+            />
+          </div>
+          <div id="boxes">
+            <input type="email" id="name" placeholder="Enter your email" />
+          </div>
+          <div id="boxes">
+            <input
+              type="password"
+              id="name"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div id="boxes">
+            <input
+              type="password"
+              id="name"
+              placeholder="please re-enter the password"
+            />
+          </div>
+          <div>
+            <input type="submit" value="SIGN UP" id="submit" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
