c7d0612fc51675d462be542169cc0c22e9aa4c6d
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-03-31 08:22:43 +0530

    .

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -13,7 +13,7 @@ function App() {
         {/* <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/casignup" element={<CASignup/>} />
-        </Routes> */} <SUSignup />
+        </Routes> */}
 
       </div>
     </Router>
