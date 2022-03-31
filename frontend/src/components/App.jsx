d9778cd7e93a333bd30164e7fcd90ca15ac09c31
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-03-31 08:21:46 +0530

    student signup - Chandana Gubba

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Home from "./Home";
 import CASignup from "./ca_auth/Signup";
+import SUSignup from "./stud_auth/Signup";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         {/* <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/casignup" element={<CASignup/>} />
-        </Routes> */}
+        </Routes> */} <SUSignup />
 
       </div>
     </Router>
---
+++ frontend/src/components/stud_auth/Signup.js
@@ -0,0 +1,39 @@
+import "../style/stud_auth/main.css";
+
+function Signup() {
+  return (
+    <div className="signup">
+      <div class="wrapper">
+        <h2>Sign Up</h2>
+        <form action="#">
+          <div class="input-box">
+            <input type="text" placeholder="User Name" required />
+          </div>
+          <div class="input-box">
+            <input type="number" placeholder="Mobile Number" required />
+          </div>
+          <div class="input-box">
+            <input type="password" placeholder="Create password" required />
+          </div>
+          <div class="input-box">
+            <input type="password" placeholder="Confirm password" required />
+          </div>
+          <div class="input-box">
+            <input type="email" placeholder="Email Id" required />
+          </div>
+
+          <div class="input-box button">
+            <input type="Submit" value="Register Now" />
+          </div>
+          <div class="text">
+            <h3>
+              Already have an account? <a href="#">Login now</a>
+            </h3>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
