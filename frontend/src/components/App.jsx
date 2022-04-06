f790d81e653c4e372fe127a4b295dd2badea3ef1
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-04-06 15:52:45 +0530

    nav + home tab (mobile responsive)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -14,7 +14,6 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/casignup" element={<CASignup/>} />
